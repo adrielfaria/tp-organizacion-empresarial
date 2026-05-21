f28188a793656e1be24577ad3fde913f50c10c19
Adriel Faria <adrielfa82@gmail.com>
2026-05-21 06:05:56 +0000

    PROY-2: Generar gráfico mensual

--- scripts/analisis.py
+++ scripts/analisis.py
@@ -1,9 +1,12 @@
 import csv
+import matplotlib.pyplot as plt
 
+# Variables
 ventas_totales = 0
 ventas_por_producto = {}
 ventas_por_mes = {}
 
+# Leer CSV
 with open("datos/ventas.csv", "r", encoding="utf-8") as archivo:
     lector = csv.DictReader(archivo)
 
@@ -16,11 +19,13 @@ with open("datos/ventas.csv", "r", encoding="utf-8") as archivo:
         total = cantidad * precio
         ventas_totales += total
 
+        # Ventas por producto
         if producto in ventas_por_producto:
             ventas_por_producto[producto] += cantidad
         else:
             ventas_por_producto[producto] = cantidad
 
+        # Ventas por mes
         mes = fecha[:7]
 
         if mes in ventas_por_mes:
@@ -28,13 +33,25 @@ with open("datos/ventas.csv", "r", encoding="utf-8") as archivo:
         else:
             ventas_por_mes[mes] = total
 
+# Producto más vendido
 producto_mas_vendido = max(
     ventas_por_producto,
     key=ventas_por_producto.get
 )
 
+# Mostrar resultados
 print("Ventas totales:", ventas_totales)
 print("Producto más vendido:", producto_mas_vendido)
 
-print("Ventas por mes:")
-print(ventas_por_mes)
+# Crear gráfico
+meses = list(ventas_por_mes.keys())
+totales = list(ventas_por_mes.values())
+
+plt.figure(figsize=(8,5))
+plt.bar(meses, totales)
+plt.title("Ventas por mes")
+plt.xlabel("Mes")
+plt.ylabel("Monto vendido")
+plt.savefig("resultados/grafico_ventas.png")
+
+print("Gráfico guardado en resultados/")
